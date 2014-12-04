970ce05df1d323057afdafc8351020bcd3aa0594
Ljubica Kostova <kostova_ljubica@hotmail.com>
2014-12-04 21:35:18 +0100

    Second`

---
+++ config/autoload/local.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ljubica
+ * Date: 01.12.2014
+ * Time: 10:13
+ */
+
+return array(
+    'db' => array(
+        'username'         => '115035',
+        'password'            => 'Lozinka115035',
+    ),
+);
